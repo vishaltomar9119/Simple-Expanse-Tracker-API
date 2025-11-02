86c655f6da036b0e677278e6407372fd3918e006
tomarvishal9119 <tomarvishal9119@gmail.com>
2025-11-02 18:31:59 +0530

    for test cicd

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,5 +5,16 @@ const authController = require('../controllers/authController');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/refreshToken', authController.refreshToken);
+router.get('/check',async(req , res)=>{
+    try{
+        const {User} = require('../models/model');
+        const data = await User.find({})
+        res.status(200).json(data);
+
+    }catch(err){
+       console.log(err);
+       res.send(err)
+    }
+})
 
 module.exports = router;
